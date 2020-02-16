216b82187e23c431f264982cb8c8184926e23730
Menushka Weeratunga <tmweeratunga@gmail.com>
2020-02-16 17:16:22 -0500

    Removed App, replaced with Main Added working search functionality

---
+++ src/components/media.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import * as path from 'path';
+
+type MediaProps = { path: string }
+
+type MediaState = {}
+
+export class Media extends React.Component<MediaProps, MediaState> {
+    render() {
+        return (
+            <div style={{display: 'inline-block', width: 150, height: 150}} >
+                <img src={path.join(__dirname, '../../', this.props.path)} style={{display: 'block', maxWidth: '100%', maxHeight: '100%', margin: 'auto'}} />
+            </div>
+        );
+    }
+}
---
+++ src/components/search.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+type SearchProps = { onChange: (value: string) => void }
+
+type SearchState = { value: string }
+
+export class Search extends React.Component<SearchProps, SearchState> {
+    constructor(props: SearchProps) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = { value: '' };
+    }
+
+    handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({ value: event.target.value });
+        this.props.onChange(event.target.value);
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </div>
+        );
+    }
+}
